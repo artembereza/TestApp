fd40c8b7a892698a74fd7859c8f9a2f86e3fbed8
AIN427 <artem.a.bereza@gmail.com>
2016-10-09 01:32:16 +0300

    implement navigation drawer

--- app/src/main/java/com/example/mann/testapp1/MainActivity.java
+++ app/src/main/java/com/example/mann/testapp1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mann.testapp1;
 
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Spinner;
 
 import com.example.mann.testapp1.adapter.ViewPagerAdapter;
 import com.example.mann.testapp1.fragment.FirstFragment;
@@ -14,9 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     ViewPager viewPager;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
@@ -24,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         setupViewPager(viewPager);
 
+        initNavigationView();
+
+
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 
     }
 
